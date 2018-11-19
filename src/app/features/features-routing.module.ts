02abab6906241ed24c4776f2cb19744f41a5a9b3
hovandong2 <hodong718@gmail.com>
2018-11-19 14:05:02 +0700

    Initial commit

---
+++ src/app/features/features-routing.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { FeaturesComponent } from './features.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: FeaturesComponent,
+    children: [
+      {
+        path: '',
+        loadChildren: './home/home.module#HomeModule'
+      },
+      {
+        path: 'categories/:id',
+        loadChildren: './categories/categories.module#CategoriesModule'
+      }
+    ]
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+export class FeaturesRoutingModule { }
